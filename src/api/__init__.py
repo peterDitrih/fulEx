d7da6be404f3adc391d863d53c898aa891894b85
iditrich <iditrich@yandex.ru>
2022-04-15 12:52:35 +0300

    TASK 3.2 added service to update stats of users from table users once a day

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,10 +1,21 @@
+import asyncio
 import sqlalchemy as sa
+import aiohttp
 from fastapi import FastAPI
-
 import src.api.protocols
 from src.api import users, protocols, stats
 from src.database import DatabaseSettings, create_database_url
 from src.user.service import UserService
+from src.stats.service import StatsService
+from src.stats.models import StatsAddRequestV1
+
+db_settings = DatabaseSettings()
+engine = sa.create_engine(
+    create_database_url(db_settings),
+    future=True
+)
+user_service = UserService(engine)
+stats_service = StatsService(engine)
 
 
 def get_application() -> FastAPI:
@@ -17,15 +28,36 @@ def get_application() -> FastAPI:
     application.include_router(users.router)
     application.include_router(stats.router)
 
-    db_settings = DatabaseSettings()
-    engine = sa.create_engine(
-        create_database_url(db_settings),
-        future=True
-    )
-    user_service = UserService(engine)
     application.dependency_overrides[protocols.UserServiceProtocol] = lambda: user_service
     application.dependency_overrides[protocols.StatsServiceProtocol] = lambda: stats_service
     return application
 
+
+async def scan_users():
+    while True:
+        users = user_service.get_all_users()
+        async with aiohttp.ClientSession() as session:
+            for user in users:
+                async with session.get(f'https://api.github.com/users/{user.login}/repos', ssl=False) as response:
+                    users_repos = await response.json()
+                stats_service.delete_stats_by_user_id(user.id)
+                for repo in users_repos:
+                    repo_obj = StatsAddRequestV1(
+                        user_id=user.id,
+                        repo_id=repo['id'],
+                        date=repo['created_at'],
+                        stargazers=repo['stargazers_count'],
+                        forks=repo['forks'],
+                        watchers=repo['watchers'],
+                    )
+                    stats_service.add_stat(repo_obj)
+
+        await asyncio.sleep(84600)
+
+try:
+    loop = asyncio.get_running_loop()
+    asyncio.create_task(scan_users())
+except RuntimeError:
+    pass
 
 app = get_application()
